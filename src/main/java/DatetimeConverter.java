9614e9a9059ea10d80c4519b5a215f1c6a5a5411
Nguyen Thai Binh <nguyen.tb@u.nus.edu>
2026-01-24 00:22:48 +0700

    Add a utility to parse/format datetime

---
+++ src/main/java/DatetimeConverter.java
@@ -0,0 +1,73 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.format.DateTimeParseException;
+import java.time.temporal.ChronoField;
+import java.util.List;
+import java.util.Locale;
+
+public class DatetimeConverter {
+    private static final LocalDateTime NOW = LocalDateTime.now();
+
+    private static final List<DateTimeFormatter> INPUT_FORMATTERS = List.of(
+            // Full date time with year
+            DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"),
+
+            // Date without year and time
+            new DateTimeFormatterBuilder()
+                    .parseCaseInsensitive()
+                    .appendPattern("MMM d")
+                    .parseDefaulting(ChronoField.YEAR, NOW.getYear())
+                    .parseDefaulting(ChronoField.HOUR_OF_DAY, 0)
+                    .parseDefaulting(ChronoField.MINUTE_OF_HOUR, 0)
+                    .parseDefaulting(ChronoField.SECOND_OF_MINUTE, 0)
+                    .toFormatter(Locale.ENGLISH),
+
+            // Date without year with time
+            new DateTimeFormatterBuilder()
+                    .parseCaseInsensitive()
+                    .appendPattern("MMM d HH:mm")
+                    .parseDefaulting(ChronoField.YEAR, NOW.getYear())
+                    .parseDefaulting(ChronoField.SECOND_OF_MINUTE, 0)
+                    .toFormatter(Locale.ENGLISH),
+
+            // Time only
+            new DateTimeFormatterBuilder()
+                    .parseCaseInsensitive()
+                    .appendPattern("HH:mm")
+                    .parseDefaulting(ChronoField.YEAR, NOW.getYear())
+                    .parseDefaulting(ChronoField.MONTH_OF_YEAR, NOW.getMonthValue())
+                    .parseDefaulting(ChronoField.DAY_OF_MONTH, NOW.getDayOfMonth())
+                    .toFormatter(Locale.ENGLISH));
+
+    public static final DateTimeFormatter SAME_YEAR = DateTimeFormatter.ofPattern("MMM d");
+    public static final DateTimeFormatter DIFFERENT_YEAR = DateTimeFormatter.ofPattern("MMM d, yyyy");
+    public static final DateTimeFormatter SAME_YEAR_T = DateTimeFormatter.ofPattern("MMM d 'at' h:mma");
+    public static final DateTimeFormatter DIFFERENT_YEAR_T = DateTimeFormatter.ofPattern("MMM d, yyyy 'at' h:mma");
+
+    public static LocalDateTime parse(String s) throws IbatunException {
+        for (DateTimeFormatter formatter : INPUT_FORMATTERS) {
+            try {
+                return LocalDateTime.parse(s, formatter);
+            } catch (DateTimeParseException e) {
+                // Try next formatter
+            }
+        }
+        throw new IbatunException("Invalid datetime: " + s);
+    }
+
+    public static String format(LocalDateTime dateTime) {
+        DateTimeFormatter formatter;
+        boolean includeTime = dateTime.getHour() != 0 || dateTime.getMinute() != 0 || dateTime.getSecond() != 0;
+        if (dateTime.getYear() == NOW.getYear()) {
+            formatter = includeTime ? SAME_YEAR_T : SAME_YEAR;
+        } else {
+            formatter = includeTime ? DIFFERENT_YEAR_T : DIFFERENT_YEAR;
+        }
+        return dateTime.format(formatter);
+    }
+
+    private DatetimeConverter() {
+        // Prevent instantiation
+    }
+}
